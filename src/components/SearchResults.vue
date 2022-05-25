790f8f5c11227a2b7a1063cc7f341be73f964180
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-05-25 10:21:51 +0200

    add files

---
+++ src/components/SearchResults.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="root">
+    <p>Showing results for "{{ query }}"</p>
+  </div>
+</template>
+<script>
+
+import { computed } from "vue";
+export default {
+  props: {
+    query: String,
+  },
+};
+</script>>
